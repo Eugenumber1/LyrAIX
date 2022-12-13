d4c6a6d3b35ed956a5f6bdaffa992c2c903eca4b
s2422980 <y.budnyk@student.utwente.nl>
2022-12-13 13:08:41 +0100

    The neural network works now, however it works extremely slow. needs improvements!

--- helpers/NeuralNetwork.py
+++ helpers/NeuralNetwork.py
@@ -8,32 +8,38 @@ class SongCreator:
     def __init__(self, songs, avarage, n_steps=100, temp=1):
         self.songs = songs
         self.avarage = avarage
-        self.texts = list()
+        self.texts = str()
         self.titles = list()
         self.n_steps = n_steps
         self.temperature = temp
         self.new_song = str()
-        print(self.texts)
+        #print(self.texts)
 
     """
     This method populates two lists with song titles and song texts
     """
     def input(self):
         for song in self.songs:
-            self.texts.append(song.song_text)
+            self.texts += song.song_text
+            #print(type(song.song_text))
             #self.titles.append(song.song_title)
-        print(self.texts)
-        for text in self.texts:
-            print(text)
+        #print(self.texts)
+        #for text in self.texts:
+            #print(text)
 
     def create_tokenizer_texts(self):
         tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=True)
+        # print(type(self.texts))
+        # print(self.texts)
+        # for text in self.texts:
+        #     print(text[0])
         tokenizer.fit_on_texts(self.texts)
         return tokenizer
 
     def create_tokenizer_titles(self):
         tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=False)
-        tokenizer.fit_on_texts(self.titles)
+
+        tokenizer.fit_on_texts([self.titles])
         return tokenizer
 
     """
@@ -41,14 +47,27 @@ class SongCreator:
     """
     def preprocessing(self):
         self.input()
+
+        #print(self.texts)
         tokenizer_texts = self.create_tokenizer_texts()
         #tokenizer_titles = self.create_tokenizer_titles()
         max_id_texts = len(tokenizer_texts.word_index) # number of distinct chars
         dataset_size_texts = tokenizer_texts.document_count # total number of chars
+        print(tokenizer_texts.texts_to_sequences(["First"]))
+        print(tokenizer_texts.sequences_to_texts([[20, 6, 9, 8, 3]]))
+        print("max id is " + str(max_id_texts))
+        print("dataset size is " + str(dataset_size_texts))
+
         #max_id_titles = len(tokenizer_titles.word_index)
         #dataset_size_titles = tokenizer_titles.document_count
         [encoded_texts] = np.array(tokenizer_texts.texts_to_sequences([self.texts])) - 1
+        print(type(encoded_texts))
+        print(len(encoded_texts))
+
         #[encoded_titles] = np.array(tokenizer_titles.texts_to_sequences(self.titles)) - 1
+        # print(encoded_texts)
+        # for i in encoded_texts:
+        #     print(i)
         return (max_id_texts, dataset_size_texts, encoded_texts, tokenizer_texts)#, (max_id_titles, dataset_size_titles, encoded_titles, tokenizer_titles)
 
 
@@ -56,6 +75,8 @@ class SongCreator:
         max_id_texts, dataset_size_texts, encoded_texts = tup[0], tup[1], tup[2]
         train_size = dataset_size_texts * 80 // 100
         dataset = tf.data.Dataset.from_tensor_slices(encoded_texts[:train_size])
+        # for i in dataset:
+        #     print(i)
         return dataset
 
     def chop_texts_into_windows(self, dataset, max_id):
@@ -67,17 +88,16 @@ class SongCreator:
         dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
         dataset = dataset.map(lambda X_batch, Y_batch: (tf.one_hot(tf.cast(X_batch, tf.int64), depth=max_id, dtype=tf.int64), Y_batch))
         dataset = dataset.prefetch(1)
-        print(dataset)
         return dataset
 
     def create_model(self, dataset, max_id):
         model = tf.keras.models.Sequential([
-            tf.keras.layers.GRU(128, return_sequences=True, input_shape=[None, max_id],
-                             dropout=0.2, recurrent_dropout=0.2),
-            tf.keras.layers.GRU(128, return_sequences=True,
-                             dropout=0.2, recurrent_dropout=0.2),
-            tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(max_id,activation="softmax"))])
-        model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", run_eagerly=True)
+            tf.keras.layers.GRU(128, return_sequences=True, input_shape=[None, max_id], dropout=0.2, recurrent_dropout=0.2),
+            tf.keras.layers.GRU(128, return_sequences=True, dropout=0.2, recurrent_dropout=0.2),
+            tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(max_id, activation="softmax"))])
+        model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", run_eagerly=True, metrics=['accuracy'])
+        # for i in dataset:
+        #     print(i)
         history = model.fit(dataset, epochs=25)
         print(history)
         return model
